e27880d5ebed00dfc5cae2263f36d63e84f26a44
sans690 <scsmith7255@gmail.com>
2023-01-17 15:01:08 -0500

    Made update to drawing

--- Lab 02 - Draw a Picture/lab_02.py
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -15,22 +15,46 @@ arcade.set_background_color(arcade.color.BLUE)
 arcade.start_render()
 
 # Grass
-arcade.draw_lrtb_rectangle_filled(0, 800, 200, 0, arcade.color.GO_GREEN)
+# left of 0, right 799
+# top of 300, bottom 0
+arcade.draw_lrtb_rectangle_filled(0, 799, 200, 0, arcade.color.GO_GREEN)
 
 # House base
-arcade.draw_lrtb_rectangle_filled(30, 300, 200, 150, arcade.color.FAWN)
+# left 30, right 300
+# top of 200, bottom of 150
+arcade.draw_lrtb_rectangle_filled(30, 300, 200, 160, arcade.color.FAWN)
 
 # House base second color
+# left of 30, right of 300
+# top of 300, bottom 170
 arcade.draw_lrtb_rectangle_filled(30, 300, 300, 170, arcade.color.BONE)
-# House door
-arcade.draw_lrtb_rectangle_filled(140, 180, 220, 150, arcade.color.BURNT_UMBER)
 
-# House window left
+# House door
+# left of 140, right 180
+# top of 220, bottom of 150
+arcade.draw_lrtb_rectangle_filled(145, 185, 220, 160, arcade.color.BURNT_UMBER)
+
+# House window (left)
+# left of 70, right of 100
+# top of 230, bottom of 200
 arcade.draw_lrtb_rectangle_filled(70, 100, 230, 200, arcade.color.BROWN)
-# House window right
-arcade.draw_lrtb_rectangle_filled(220, 255, 230, 200, arcade.color.BROWN)
+
+# House window (right)
+# left of 220, right of 230
+# top of 230, bottom of 200
+arcade.draw_lrtb_rectangle_filled(230, 260, 230, 200, arcade.color.BROWN)
 
 # Roof from triangle with 3 points
+# (10, 300) (170, 400) (315, 300)
+arcade.draw_triangle_filled(10, 300, 170, 400, 315, 300, arcade.color.BROWN)
+
+# Lines on window (left)
+
+# Lines on window (right)
+
+
+
+
 
 
 arcade.finish_render()
