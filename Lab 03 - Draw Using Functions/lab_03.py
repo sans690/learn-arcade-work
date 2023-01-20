b8678d9ecd87a549cf9e720035707e6a8a8ab616
sans690 <scsmith7255@gmail.com>
2023-01-19 19:00:34 -0500

    start on drawing for lab 3

--- Lab 03 - Draw Using Functions/lab_03.py
+++ Lab 03 - Draw Using Functions/lab_03.py
@@ -24,17 +24,53 @@ def draw_face(x, y):
     arcade.draw_rectangle_outline(120 + x, 105 + y, screen_width / 30, screen_height / 35, arcade.color.BLACK, 3)
     arcade.draw_line(75 + x, 105 + y, 105 + x, 105 + y, arcade.color.BLACK, 3)
 
+
+def on_draw(delta_time):
+    """Draws everything"""
+    arcade.start_render()
+    draw_face(on_draw.face1_x, 140)
+    draw_face(on_draw.face2_x, 531)
+    draw_face(on_draw.face3_x, 622)
+    draw_face(on_draw.face4_x, 255)
+
+    draw_face(408, on_draw.face5_y)
+    draw_face(350, on_draw.face6_y)
+    draw_face(500, on_draw.face7_y)
+    draw_face(600, on_draw.face8_y)
+
+    # Negative number moves left, positive moves right
+    on_draw.face1_x += 13
+    on_draw.face2_x -= 10
+    on_draw.face3_x -= 6
+    on_draw.face4_x -= 8
+    # Negative number moves down, positive numbers move up
+    on_draw.face5_y -= 13
+    on_draw.face6_y += 15
+    on_draw.face7_y -= 10
+    on_draw.face8_y += 11
+
+
+# X will start on at:
+on_draw.face1_x = 150
+on_draw.face2_x = 200
+on_draw.face3_x = 300
+on_draw.face4_x = 600
+
+# Y will start at:
+on_draw.face5_y = 800
+on_draw.face6_y = 200
+on_draw.face7_y = 205
+on_draw.face8_y = 302
+
+
 def main():
     arcade.open_window(screen_width, screen_height, "Drawing Example")
     arcade.set_background_color(arcade.color.AIR_SUPERIORITY_BLUE)
-    draw_face(106, 362)
-    draw_face(325, 250)
-    draw_face(614, 622)
-    draw_face(821, 255)
 
     # To run and finish program
-    arcade.finish_render()
+    arcade.schedule(on_draw, 1 / 50)
     arcade.run()
 
 
+# Calls main function to start
 main()
