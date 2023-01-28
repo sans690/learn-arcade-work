21501c73169ab9a7fad6e4b4ad60b2e611c30bea
sans690 <scsmith7255@gmail.com>
2023-01-27 19:51:47 -0500

    Redo of lab 4, needs to have last section fixed

--- Lab 04 - Camel/lab_04.py
+++ Lab 04 - Camel/lab_04.py
@@ -1,33 +1,107 @@
+import random
+
 print("Welcome to Camel! You have stolen a camel to make your way across the great Mobi desert.")
 print("The natives want their camel back and are chasing you down! ")
 print("Survive your desert trek and out run the natives.")
 
-miles_traveled = 0
-thirst = 0
-camel_tiredness = 0
-natives_behind = -20
-drinks_in_canteen = 3
-done = False
+DONE = False
 
 
-def program():
-    while not done:
-        print("A. Drink from your canteen.")
-        print("B. Ahead moderate speed.")
-        print("C. Ahead full speed.")
-        print("D. Stop for the night.")
-        print("E. Status check.")
-        print("Q. Quit.")
-        userInput = input("What is your choice? ")
-        if userInput.upper() == "Q":
+def main():
+    MILES_TRAVELED = 0
+    CAMEL_TIREDNESS = 0
+    NATIVES_BEHIND_YOU = -20
+    DRINKS_IN_CANTEEN = 0
+    NATIVES_UP = random.randrange(7, 15)
+    FULL_SPEED = random.randrange(10, 21)
+    CAMEL_USES_ENERGY = + random.randrange(1, 4)
+    MODERATE_SPEED = random.randrange(5, 13)
+    DRINKS_IN_CANTEEN = 3
+    YES_DRINK = DRINKS_IN_CANTEEN - 1
+    CAMEL_THIRST = 0
+    USER_THIRST = 0
+    # choices for user to pick from
+    print("A. Drink from your canteen. ")
+    print("B. Ahead moderate speed. ")
+    print("C. Ahead full speed. ")
+    print("D. Stop for the night. ")
+    print("E. Status check. ")
+    print("Q. Quit. ")
+    # user is asked question and gives a response
+    USER_INPUT = input("What is your choice? ")
+    # user picks e = status
+    if USER_INPUT.upper() == "E":
+        print("Miles traveled:", MILES_TRAVELED)
+        print("Drinks in canteen:", DRINKS_IN_CANTEEN)
+        # miles that natives are behind user
+        NATIVES_BEHIND_YOU_NOW = MILES_TRAVELED - NATIVES_BEHIND_YOU
+        print("The natives are", NATIVES_BEHIND_YOU_NOW, "miles behind you.")
+    # user picks q = quit
+    elif USER_INPUT.upper() == "Q":
+        # End game
+        DONE = True
+    # user picks d = rest for night
+    elif USER_INPUT.upper() == "D":
+        CAMEL_TIREDNESS = 0
+        print("Camel is happy!")
+        NATIVES_UP = random.randrange(7, 15)
+        print("The native are now", NATIVES_UP, "miles behind you.")
+    # user picks c = full speed
+    elif USER_INPUT.upper() == "C":
+        print("You travel", FULL_SPEED, "miles")
+        print("Camel's thirst is", CAMEL_THIRST + 1)
+        print("Your thirst is", USER_THIRST + 1)
+        print("Camel's tiredness is", CAMEL_TIREDNESS + 1)
+        NATIVES_UP = random.randrange(7, 15)
+        print("Natives move up", NATIVES_UP, "miles")
+    # user picks b = moderate speed
+    elif USER_INPUT.upper() == "B":
+        print("You travel", MODERATE_SPEED, "miles.")
+        print("Camel's thirst is", CAMEL_THIRST + 1)
+        print("Your thirst is", USER_THIRST + 1)
+        print("Natives move up", NATIVES_UP, "miles")
+    # user picks a = drink from canteen
+    elif USER_INPUT.upper() == "A":
+        USER_INPUT_YORN = input("Do you take a drink? ")
+        if USER_INPUT_YORN.upper() == "YES":
+            print("You have", YES_DRINK, "drinks left in canteen. ")
+            print("Your thirst is", USER_THIRST)
+        else:
+            print("ERROR. ")
+        # you are thirsty
+        if USER_THIRST > 4:
+            print("You are thirsty! ")
+        # you die of thirst
+        if USER_THIRST > 6:
+            print("You died of thirst! ")
             exit()
+        # camel is tired
+        if CAMEL_TIREDNESS > 5:
+            print("Your camel is getting tired. ")
+        # camel dies of exhaustion
+        if CAMEL_TIREDNESS > 8:
+            print("Your camel is dead of exhaustion!")
+        # natives caught you
+        if NATIVES_BEHIND_YOU == 0:
+            print("The natives have caught up! ")
+            exit()
+        # camel is thirsty
+        if CAMEL_THIRST > 5:
+            print("Camel getting thirsty! ")
+        # camel dies of thirst
+        if CAMEL_THIRST > 8:
+            print("Camel died of thirst! ")
+        # you beat the game
+        if MILES_TRAVELED == 200:
+            print("You win!")
+            exit()
+        # 1 / 20 of finding an oasis
+        for i in range(1):
 
-        elif userInput.upper() == "E":
-            print("Miles traveled: ", miles_traveled)
-            print("Drinks in canteen: ", drinks_in_canteen)
-            print("Your camel has ", camel_tiredness, "amount of fatigue.")
-            print("The natives are ", natives_behind, "miles behind you.")
-            print()
+            if random.randrange(21) == 1:
+                print("You found an oasis! ")
+            else:
+                print("Found nothing. ")
 
 
-program()
+main()
